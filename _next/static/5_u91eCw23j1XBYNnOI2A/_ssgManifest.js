6a789cdb33ee0f791388a02a0ed1cb3cdf923eb5
pierreantoinedescoteaux-ui <pierreantoinedescoteaux@gmail.com>
2026-08-05 22:38:30 -0400

    psych-map static site — Map V2 design sprint build 2026-08-05

---
+++ _next/static/5_u91eCw23j1XBYNnOI2A/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fapproaches\u002F[slug]","\u002Feffects\u002F[slug]","\u002Fgoals\u002F[slug]","\u002Fmodels\u002F[slug]","\u002Fthemes\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
